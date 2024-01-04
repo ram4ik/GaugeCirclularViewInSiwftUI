1cea0084c8bc905840bbc923bf8a654f30ce2d5f
ram4ik <ram4ik@hotmail.com>
2024-01-04 19:42:11 +0200

    Initial Commit

---
+++ GaugeCirclularViewInSiwftUI/GaugeCirclularViewInSiwftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GaugeCirclularViewInSiwftUIApp.swift
+//  GaugeCirclularViewInSiwftUI
+//
+//  Created by Ramill Ibragimov on 04.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct GaugeCirclularViewInSiwftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
