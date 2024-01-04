e7d4680b43a1d5a7f9b3af216f645959aa3c84d1
ram4ik <ram4ik@hotmail.com>
2024-01-04 20:17:40 +0200

    init

--- GaugeCirclularViewInSiwftUI/ContentView.swift
+++ GaugeCirclularViewInSiwftUI/ContentView.swift
@@ -8,14 +8,83 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var progress: Double = 0.25
+    
+    let gradient = Gradient(colors: [.blue, .purple, .pink, .red, .green])
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack {
+                Gauge(value: progress, label: {
+                    Text(String(format: "%.1f", progress))
+                })
+                    .gaugeStyle(.accessoryCircular)
+                    .tint(gradient)
+                    .scaleEffect(2.4)
+                    .padding()
+                
+                Gauge(value: progress, label: {
+                    Text("\(progress)")
+                })
+                    .gaugeStyle(.automatic)
+                    .tint(.orange)
+                    .padding()
+                
+                Gauge(value: progress, label: {
+                    Text(String(format: "%.1f", progress))
+                })
+                    .gaugeStyle(.accessoryCircular)
+                    .tint(.indigo)
+                    .padding()
+                
+                Gauge(value: progress, label: {
+                    Text("\(progress)")
+                })
+                    .gaugeStyle(.accessoryCircularCapacity)
+                    .tint(.mint)
+                    .padding()
+                
+                Gauge(value: progress, label: {
+                    Text("\(progress)")
+                })
+                    .gaugeStyle(.accessoryLinear)
+                    .tint(.cyan)
+                    .padding()
+                
+                Gauge(value: progress, label: {
+                    Text("\(progress)")
+                })
+                    .gaugeStyle(.accessoryLinearCapacity)
+                    .tint(.teal)
+                    .padding()
+                
+                Gauge(value: progress, label: {
+                    Text("\(progress)")
+                })
+                    .gaugeStyle(.linearCapacity)
+                    .tint(.pink)
+                    .padding()
+            }
+            .navigationTitle("Gauge")
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button {
+                        progress += 0.05
+                    } label: {
+                        Text("+")
+                    }
+                    .buttonStyle(.borderedProminent)
+                }
+                ToolbarItem(placement: .topBarLeading) {
+                    Button {
+                        progress -= 0.05
+                    } label: {
+                        Text("-")
+                    }
+                    .buttonStyle(.borderedProminent)
+                }
+            }
         }
-        .padding()
     }
 }
 
